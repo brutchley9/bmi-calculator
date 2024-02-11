9796021418dc484e003c1c1326ca439b07cbaa5c
brutchley <bhutchison0901@yahoo.com>
2024-02-11 16:45:08 -0600

    added logic, commented out, that shows how a JS function/variable would successfully calculate BMI. Now to create a function with this based on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+//Equation to calculate BMI from height in feet/inches and weight in lbs
+//BMI = [weight(lb) / height(in)^2] * 703
+
+//Logic below shows how a variable or function would calculate BMI using JS.
+
+/*var feet = 5;
+var inches = 7;
+var inchHeight = (feet * 12) + inches;
+var pounds = 175;
+var weightBMI = (pounds * 703) / (inchHeight * inchHeight)
+console.log(weightBMI);*/
+
+function calculateBMI() {
+    console.log("It works!")
+};
+
+calculateBMI();
